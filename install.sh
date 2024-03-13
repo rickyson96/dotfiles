e1a5e3e6b28f4b8fbdd2a8a4a99006a4cdfaec59
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-03-13 08:53:21 +0000

    emacs snapshot

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
+sudo apt-add-repository ppa:ubuntu-elisp/ppa
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y fish emacs ripgrep stow
+sudo apt install -y fish emacs-snapshot ripgrep stow
 
 stow -t "$HOME" . 
