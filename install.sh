90d968a184c53e0441863f597599623b06e7ea96
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-03-13 12:56:21 +0000

    manual install emacs

--- install.sh
+++ install.sh
@@ -1,7 +1,42 @@
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-add-repository ppa:ubuntu-elisp/ppa
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
+
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y fish emacs-snapshot ripgrep stow
+sudo apt install -y fish ripgrep stow
+
+# install emacs build tools
+sudo apt install -y build-essential gcc-10 libgccjit-10-dev texinfo
+
+# install tree-sitter
+git clone https://github.com/tree-sitter/tree-sitter.git
+pushd tree-sitter || exit
+make
+sudo make install
+popd
+
+# install emacs
+git clone https://git.savannah.gnu.org/git/emacs.git
+pushd emacs || exit
+export CC=/usr/bin/gcc-10 \
+       CXX=/usr/bin/gcc-10 \
+       LD_LIBRARY_PATH=/usr/local/lib
+       
+./configure \
+    --with-tree-sitter \
+    --with-native-compilation \
+    --without-compress-install \
+    --with-x-toolkit=no \
+    --with-jpeg=ifavailable \
+    --with-xpm=ifavailable \
+    --with-tiff=ifavailable \
+    --with-png=ifavailable \
+    --with-gif=ifavailable \
+    CC=gcc-10
+
+make bootstrap
+make
+
 
 stow -t "$HOME" . 
