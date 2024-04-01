ff49ecd134d1d61c6b36d1bc0429818bf76033a1
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-04-01 13:41:49 +0700

    emacs: fix indentation

--- .emacs.d/lisp/+elpaca.el
+++ .emacs.d/lisp/+elpaca.el
@@ -53,13 +53,13 @@
 (elpaca `(,@elpaca-order))
 
 (defun +elpaca-unload-seq (e) "Unload seq before continuing the elpaca build, then continue to build the recipe E."
-  (and (featurep 'seq) (unload-feature 'seq t))
-  (elpaca--continue-build e))
+       (and (featurep 'seq) (unload-feature 'seq t))
+       (elpaca--continue-build e))
 
 (elpaca `(seq :build ,(append (butlast (if (file-exists-p (expand-file-name "seq" elpaca-builds-directory))
-                                          elpaca--pre-built-steps
-                                        elpaca-build-steps))
-                             (list '+elpaca-unload-seq 'elpaca--activate-package))))
+                                           elpaca--pre-built-steps
+                                         elpaca-build-steps))
+                              (list '+elpaca-unload-seq 'elpaca--activate-package))))
 
 ;; I put system-packages here because it's also package manager but systemwide.
 (elpaca system-packages
