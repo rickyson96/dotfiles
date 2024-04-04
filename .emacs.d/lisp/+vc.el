9eadb619b6b167c5d709b022894830ff4ecb9b82
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-04-04 16:04:30 +0000

    emacs: defhydra autoloaded, do not need with-eval-after-load

--- .emacs.d/lisp/+vc.el
+++ .emacs.d/lisp/+vc.el
@@ -28,10 +28,9 @@
 	  '("B" "branch/revision" magit-checkout)))
 
   ;; Taken from https://github.com/alphapapa/unpackaged.el#hydra
-  (with-eval-after-load 'hydra
-	(defhydra smerge-hydra
-	  (:color pink :hint nil :post (smerge-auto-leave))
-	  "
+  (defhydra smerge-hydra
+	(:color pink :hint nil :post (smerge-auto-leave))
+	"
 ^Move^       ^Keep^               ^Diff^                 ^Other^
 ^^-----------^^-------------------^^---------------------^^-------
 _n_ext       _b_ase               _<_: upper/base        _C_ombine
@@ -40,31 +39,31 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 ^^           _a_ll                _R_efine
 ^^           _RET_: current       _E_diff
 "
-	  ("n" smerge-next)
-      ("p" smerge-prev)
-      ("b" smerge-keep-base)
-      ("u" smerge-keep-upper)
-      ("l" smerge-keep-lower)
-      ("a" smerge-keep-all)
-      ("RET" smerge-keep-current)
-      ("\C-m" smerge-keep-current)
-      ("<" smerge-diff-base-upper)
-      ("=" smerge-diff-upper-lower)
-      (">" smerge-diff-base-lower)
-      ("R" smerge-refine)
-      ("E" smerge-ediff)
-      ("C" smerge-combine-with-next)
-      ("r" smerge-resolve)
-      ("k" smerge-kill-current)
-      ("ZZ" (lambda ()
-              (interactive)
-              (save-buffer)
-              (bury-buffer))
-       "Save and bury buffer" :color blue)
-	  ("q" nil "cancel" :color blue))
+	("n" smerge-next)
+	("p" smerge-prev)
+	("b" smerge-keep-base)
+	("u" smerge-keep-upper)
+	("l" smerge-keep-lower)
+	("a" smerge-keep-all)
+	("RET" smerge-keep-current)
+	("\C-m" smerge-keep-current)
+	("<" smerge-diff-base-upper)
+	("=" smerge-diff-upper-lower)
+	(">" smerge-diff-base-lower)
+	("R" smerge-refine)
+	("E" smerge-ediff)
+	("C" smerge-combine-with-next)
+	("r" smerge-resolve)
+	("k" smerge-kill-current)
+	("ZZ" (lambda ()
+			(interactive)
+			(save-buffer)
+			(bury-buffer))
+	 "Save and bury buffer" :color blue)
+	("q" nil "cancel" :color blue))
 
-	(add-hook 'smerge-mode-hook (lambda () (when smerge-mode
-											 (smerge-hydra))))))
+  (add-hook 'smerge-mode-hook (lambda () (when smerge-mode
+										   (smerge-hydra/body)))))
 
 (elpaca magit-imerge)
 
