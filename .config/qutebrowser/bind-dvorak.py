f9eca1c36a94598c06408736d095926e18f0e9e6
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-04-19 12:01:22 +0700

    qb: update dvorak bindings

--- .config/qutebrowser/bind-dvorak.py
+++ .config/qutebrowser/bind-dvorak.py
@@ -1,6 +1,8 @@
 normal_bindings = {
     'N': 'tab-next',
     'P': 'tab-prev',
+    '<alt-n>': 'tab-next',
+    '<alt-p>': 'tab-prev',
     'n': 'scroll down',
     'p': 'scroll up',
     'h': 'scroll left',
@@ -13,11 +15,21 @@ normal_bindings = {
     '<alt-,>': 'scroll-to-perc 0',
     'v': 'scroll-page 0 0.5',
     'V': 'scroll-page 0 -0.5',
-    'k': 'tab-close',
+    '<ctrl-v>': 'scroll-page 0 0.5',
+    '<alt-v>': 'scroll-page 0 -0.5',
+    'K': 'tab-close',
+
+    # reload
+    'r': 'reload',
+    'R': 'reload -f',
+    '<alt-r>': 'reload',
+    '<alt-shift-r>': 'reload -f',
 
     # open
     'o': 'cmd-set-text -s :open',
     'O': 'cmd-set-text -s :open -t',
+    '<ctrl-o>': 'cmd-set-text -s :open',
+    '<ctrl-O>': 'cmd-set-text -s :open -t',
     '<Alt-o>o': 'cmd-set-text -s :open -b',
     '<Alt-o>O': 'cmd-set-text -s :open -b',
     '<Alt-o>l': 'cmd-set-text :open {url:pretty}',
@@ -29,7 +41,22 @@ normal_bindings = {
     'E': 'hint all tab',
     '<ctrl-e>': 'hint inputs --first',
     '<alt-e>i': 'hint inputs',
-    '<alt-e>y': 'hint links yank'
+    '<alt-e>y': 'hint links yank',
+    '<alt-e>r': 'hint --rapid links tab-bg',
+
+    # toggle
+    '<alt-t><alt-t>': 'config-cycle -t tabs.show switching always',
+
+    # search
+    '/': 'cmd-set-text /',
+    's': 'search-next',
+    'S': 'search-prev',
+
+    # yank
+    '<alt-w>': 'yank',
+    '<alt-shift-w>p': 'yank pretty-url',
+    '<ctrl-y>': 'open -- {clipboard}',
+    '<alt-y>t': 'open -t -- {clipboard}'
 }
 
 for k,v in normal_bindings.items():
