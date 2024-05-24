149ebfe228a9a8c114d753b83d545653844965c5
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-05-24 23:17:10 +0700

    emacs: magit transient default update-refs

--- .emacs.d/etc/transient/values.el
+++ .emacs.d/etc/transient/values.el
@@ -1,1 +1,2 @@
-((magit-log:magit-log-mode "-n128" "--graph" "--color" "--decorate"))
+((magit-log:magit-log-mode "-n128" "--graph" "--color" "--decorate")
+ (magit-rebase "--update-refs" "--autostash"))
